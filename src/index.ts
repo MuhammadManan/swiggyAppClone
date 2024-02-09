f14bd31e8b440d73f0e69c3328ce048270542e52
Manan Azhar <muhammadmananazhar1212@gmail.com>
2024-02-09 15:46:55 +0500

    learning Promise

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,36 @@
-import * as express from 'express';
- 
+import * as express from "express";
+
 let app: express.Application = express();
 
-
-app.listen(3000,()=>{
-    console.log('Server is running at port:3000');
+app.listen(3000, () => {
+  console.log("Server is running at port:3000");
 });
+
+function isMathsByXAvailable() {
+  return false;
+}
+
+function isMathsByYAvailable() {
+  return false;
+}
+
+function result(): Promise<string> {
+  return new Promise((resolve, reject) => {
+    if (isMathsByXAvailable()) {
+      resolve("Maths by X available");
+    } else if (isMathsByYAvailable()) {
+      resolve("Maths by Y available");
+    } else {
+      reject("Both books are not available");
+    }
+  });
+}
+
+result()
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    // console.log("hello");
+    console.log("Error:", error);
+  });
