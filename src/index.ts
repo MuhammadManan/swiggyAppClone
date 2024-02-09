5a8c84d640557f08332b886ee54d28a6fc9a924c
Manan Azhar <muhammadmananazhar1212@gmail.com>
2024-02-09 18:46:10 +0500

    spread operator on array

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,16 @@ app.listen(3000, () => {
   console.log("Server is running at port:3000");
 });
 
-const obj1 = { id : 1, name: 'manan'};
-const obj2 = {...obj1, email: 'manan@hotmail.com', name : 'ali'};
-console.log(obj1);
-console.log(obj2);
+const obj1 = { id: 1, name: "manan" };
+const obj2 = { email: "manan@hotmail.com", name: "ali" };
+const obj3 = { ...obj1, ...obj2 };
+console.log(obj3);
+// console.log(obj1);
+// console.log(obj2);
+// bd mei ana wala object(using spread operator) phely waly objects ki value update krdy ga
+// but array mei aesa khuch ni hota 
+
+const arr1 = [1,2,3];
+const arr2 = [2,3,4];
+const arr3 = [...arr1, ...arr2, {...obj3}];
+console.log(arr3);
