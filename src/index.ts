57df71ba1d78152b31a95650cdd250bfd2ed8515
Manan Azhar <muhammadmananazhar1212@gmail.com>
2024-02-09 21:41:37 +0500

    connected with mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import * as express from "express";
+import * as mongoose from 'mongoose';
 
 let app: express.Application = express();
 
 app.listen(3000, () => {
   console.log("Server is running at port:3000");
+});
+
+mongoose.connect(
+  "mongodb+srv://muhammadmananazhar1212:0MBV0ANgFNozoPaG@foodapi.ymyrbdf.mongodb.net/"
+).then(() =>{
+  console.log('Connected to Mongodb');
 });
 
 app.use((req,res,next)=>{
@@ -14,11 +21,7 @@ app.use((req,res,next)=>{
 app.get("/api/user/login", (req, res, next) => {
   // console.log(req);
   console.log(req.query.email);
-  next();
-}, (req, res, next) =>{
-  console.log('middleware2');
-  res.send("success!");
-  // next();
+  res.status(200).send('success!');
 });
 
 app.get("/api/owner/login", (req, res) => {
@@ -29,3 +32,4 @@ app.get("/api/owner/login", (req, res) => {
     array: ['test'],
   });
 });
+
