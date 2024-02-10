0cc12ce30c09edffffaffbc93fcbbe5a665f4c2c
Manan Azhar <muhammadmananazhar1212@gmail.com>
2024-02-10 14:20:47 +0500

    Structuring nodejs project

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,12 @@
-import * as express from "express";
-import * as mongoose from 'mongoose';
-import { getEnvironmentVariables } from "./environments/environment";
+import { Server } from "./server";
 
-let app: express.Application = express();
+let server = new Server().app;
+let port = 3000;
 
-app.listen(3000, () => {
-  console.log("Server is running at port:3000");
+server.listen(port, () => {
+  console.log(`Server is running at port:${port}`);
 });
 
-mongoose.connect(getEnvironmentVariables().db_uri).then(() =>{
-  console.log('Connected to Mongodb');
-});
 
-app.use((req,res,next)=>{
-  console.log('middleware1');
-  next();
-})
 
-app.get("/api/user/login", (req, res, next) => {
-  // console.log(req);
-  console.log(req.query.email);
-  res.status(200).send('success!');
-});
-
-app.get("/api/owner/login", (req, res) => {
-  res.status(201).json({
-    name: "Ali",
-    age: 21,
-    email: "ali@gmail.com",
-    array: ['test'],
-  });
-});
+ 
